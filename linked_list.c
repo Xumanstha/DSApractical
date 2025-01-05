9386d23744d12b19edb45eb9418cc7bbe00c95e6
Xumanstha <shresthaangura@gmail.com>
2025-01-05 16:53:39 +0545

    linked list

--- linked_list.c
+++ linked_list.c
@@ -20,13 +20,13 @@ void InsertAtBeginning(int value)
     {
         node->next = NULL; // If empty, the new node points to NULL
         head = node;       // Make the new node the head
-        printf("Number inserted:%d", value);
+        printf("Number inserted:%d\n", value);
     }
     else
     {
         node->next = head; // Point the new node to the current head
         head = node;       // Update the head to the new node
-        printf("Number inserted:%d", value);
+        printf("Number inserted:%d\n", value);
     }
 }
 
@@ -39,7 +39,7 @@ void InsertAtLast(int value)
     if (head == NULL)                  // Check if the list is empty
     {
         head = node; // If empty, the new node becomes the head
-        printf("Number inserted:%d", value);
+        printf("Number inserted:%d\n", value);
     }
     else
     {
@@ -49,7 +49,7 @@ void InsertAtLast(int value)
             temp = temp->next;
         }
         temp->next = node; // Set the last node's next to the new node
-        printf("Number inserted:%d", value);
+        printf("Number inserted:%d\n", value);
     }
 }
 
@@ -63,7 +63,7 @@ void InsertAtX(int value, int loc)
     {
         node->next = head; // Point the new node to the current head
         head = node;       // Update the head to the new node
-        printf("Number inserted:%d", value);
+        printf("Number inserted:%d\n", value);
         return;
     }
     Node *temp = head;        // Temporary pointer to traverse the list
@@ -86,7 +86,7 @@ void InsertAtX(int value, int loc)
     }
     node->next = temp->next; // Point the new node to the next node
     temp->next = node;       // Update the (loc - 1)th node to point to the new node
-    printf("Number inserted:%d", value);
+    printf("Number inserted:%d\n", value);
 }
 
 // Function to traverse and print the list
@@ -131,8 +131,8 @@ int main()
     int choice, value, position;
     do
     {
-        printf("1.Insert At Beginning\n2.Insert At Last\n3.Insert At specific position\n4.Delete At beginning\n5.Display List\n6.Exit");
-        printf("ENter your choice:");
+        printf("1.Insert At Beginning\n2.Insert At Last\n3.Insert At specific position\n4.Delete At beginning\n5.Display List\n6.Exit\n");
+        printf("Enter your choice:");
         scanf("%d", &choice);
         switch (choice)
         {
@@ -141,7 +141,7 @@ int main()
             scanf("%d", &value);
             InsertAtBeginning(value);
             break;
-        case2:
+        case 2:
             printf("Enter the number to add to List:");
             scanf("%d", &value);
             InsertAtLast(value);
